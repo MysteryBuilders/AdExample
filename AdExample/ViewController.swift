57d928e719a938f4ce0298d566b301c8569e6907
MysteryBuilders <eng.melshwehy@gmail.com>
2018-01-24 18:10:12 +0300

    hi

---
+++ AdExample/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  AdExample
+//
+//  Created by MohammedElshwehy on 1/24/18.
+//  Copyright © 2018 MohammedElshwehy. All rights reserved.
+//
+
+import UIKit
+
+import GoogleMobileAds
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var bannerView: GADBannerView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bannerView.adUnitID = "ca-app-pub-1538489617366643~9846760584"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
